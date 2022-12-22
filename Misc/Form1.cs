120b65635a505d0ab9a469f6eaced2d0173fd3f8
vishw <vishw@MSI>
2022-12-22 15:18:10 -0500

    Add project files.

---
+++ Misc/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Azure.ServiceBus;
+using Azure;
+using Azure.Search.Documents;
+using Azure.Search.Documents.Indexes;
+using Azure.Search.Documents.Indexes.Models;
+using Azure.Search.Documents.Models;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using StackExchange.Redis;
+using DocumentFormat.OpenXml.Drawing.Diagrams;
+
+namespace Misc
+{
+    public partial class Form1 : Form
+    {
+        const string connStr = "ConnStr";
+        const string queueName = "QueueName";
+        static IQueueClient queueClient;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //queueClient = new QueueClient(connStr, queueName);
+            string serviceName = "<your-search-service-name>";
+            string apiKey = "<your-search-service-admin-api-key>";
+            string indexName = "hotels-quickstart";
+
+            // Create a SearchIndexClient to send create/delete index commands
+            Uri serviceEndpoint = new Uri($"https://{serviceName}.search.windows.net/");
+            AzureKeyCredential credential = new AzureKeyCredential(apiKey);
+            SearchIndexClient adminClient = new SearchIndexClient(serviceEndpoint, credential);
+
+            // Create a SearchClient to load and query documents
+            SearchClient srchclient = new SearchClient(serviceEndpoint, indexName, credential);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CloudStorageAccount storage = CloudStorageAccount.Parse("ConnStr");
+            CloudTableClient tableClient = storage.CreateCloudTableClient();
+            CloudTable table = tableClient.GetTableReference("Table");
+
+            TableBatchOperation tableOperations = new TableBatchOperation();
+            table.ExecuteBatch(tableOperations);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //IDatabase cache = Connection.GetDatabase();
+        }
+    }
+}
---
+++ Misc/InvoiceRepository.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misc
+{
+    public class InvoiceRepository
+    {
+        IQueryable<Invoice> _invoices;
+        public InvoiceRepository(IQueryable<Invoice> invoices)
+        {
+            // Console.WriteLine("Sample debug output");
+            this._invoices = invoices;
+        }
+
+        /// <summary>
+        /// Should return a total value of an invoice with a given id. If an invoice does not exist null should be returned.
+        /// </summary>
+        /// <param name="invoiceId"></param>
+        /// <returns></returns>
+        public decimal? GetTotal(int invoiceId)
+        {
+            decimal total = 0;
+
+            var t = _invoices.Where(x => x.Id == invoiceId)
+                .Select(x => x.InvoiceItems);
+
+            foreach(var x in t)
+            {
+
+                var r = t.
+            }
+
+            foreach (var invoice in _invoices)
+            {
+
+                if (invoice.Id == invoiceId)
+                {
+                    var invoiceItem = invoice.InvoiceItems;
+                    foreach (var ini in invoiceItem)
+                    {
+                        total = total + ini.Price;
+
+                    }
+
+                    return total;
+
+                }
+
+            }
+
+            return total;
+        }
+
+        /// <summary>
+        /// Should return a total value of all unpaid invoices.
+        /// </summary>
+        /// <returns></returns>
+        public decimal GetTotalOfUnpaid()
+        {
+            decimal total = 0;
+            foreach (var invoice in _invoices)
+            {
+
+                if (invoice.AcceptanceDate == null)
+                {
+                    var invoiceItem = invoice.InvoiceItems;
+                    foreach (var ini in invoiceItem)
+                    {
+                        total = total + ini.Price;
+
+                    }
+
+
+
+                }
+
+            }
+
+            return total;
+        }
+
+        /// <summary>
+        /// Should return a dictionary where the name of an invoice item is a key and the number of bought items is a value.
+        /// The number of bought items should be summed within a given period of time (from, to). Both the from date and the end date can be null.
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <returns></returns>
+        public IReadOnlyDictionary<string, long> GetItemsReport(DateTime? from, DateTime? to)
+        {
+            Dictionary<string, long> dic = new Dictionary<string, long>();
+            foreach (var invoice in _invoices)
+            {
+
+
+                var invoiceItem = invoice.InvoiceItems;
+                foreach (var ini in invoiceItem)
+                {
+                    var thisTag = dic.FirstOrDefault(t => t.Key == ini.Name);
+                    if (thisTag.Value <= 0)
+                    {
+                        var tot = Convert.ToDecimal(thisTag.) + ini.Price;
+                        dic.Remove(ini.Name);
+                        dic.Add(ini.Name, tot);
+                    }
+                    else
+
+                        dic.Add(ini.Name, ini.Count);
+
+                }
+
+
+
+
+
+            }
+
+            return dic;
+        }
+    }
+}
